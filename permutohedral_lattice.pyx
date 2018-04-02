7cef2bf7270c738e414a367d1084dd11f4c7ff16
xtudbxk <xtudbxk@126.com>
2018-04-02 17:12:02 +0800

    add batch support in the cython level; maybe we can dig deeper to add it in the c++ level some day

--- permutohedral_lattice.pyx
+++ permutohedral_lattice.pyx
@@ -3,13 +3,23 @@ import numpy as np
 cdef class pyPermutohedralLattice:
     @staticmethod
     def filter(float[:,:,:,::1] a ,float[:,:,:,::1] b):
-    #def filter(float[:,:,:,::1] a not None,float[:,:,:,::1] b not None):
-        a_pyImage = image.pyImage().set_data(a)
-        b_pyImage = image.pyImage().set_data(b)
-        r = PermutohedralLattice.filter(image.pyImage.get_img(a_pyImage),image.pyImage.get_img(b_pyImage))
-        r_pyImage = image.pyImage().set_img(r)
-        ret = np.asarray(r_pyImage)
-        ret = np.copy(ret,order="C")
-        a_pyImage.mydel()
-        b_pyImage.mydel()
+        # a: the matric needs to be filered which shape is [batch_size,:,:,:]. b: the feature matric, which shape is [1,:,:,:]
+        batch_size = a.shape[0]
+        h = a.shape[1]
+        w = a.shape[2]
+        c = a.shape[3]
+        ret = None
+        for i in range(batch_size): 
+            a_pyImage = image.pyImage().set_data(np.reshape(a[i,:,:,:],(1,h,w,c)))
+            b_pyImage = image.pyImage().set_data(b)
+            r = PermutohedralLattice.filter(image.pyImage.get_img(a_pyImage),image.pyImage.get_img(b_pyImage))
+            r_pyImage = image.pyImage().set_img(r)
+            ret_tmp = np.asarray(r_pyImage)
+            ret_tmp = np.copy(ret_tmp,order="C")
+            a_pyImage.mydel()
+            b_pyImage.mydel()
+            if ret is None:
+                ret = ret_tmp
+            else:
+                ret = np.concatenate([ret,ret_tmp],axis=0)
         return ret
